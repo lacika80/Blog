7440ecc63238072bbfe8b822227da94586972d84
Laszlo Gyurkovics <gyurkovics95@gmail.com>
2021-09-13 15:04:23 +0200

    folder structure change, plugin implements

---
+++ app/views/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="themes/frontend/js/jquery.min.js"></script>
+        <script src="themes/frontend/vendors/swiper/swiper.jquery.min.js"></script>
+        <script src="themes/frontend/js/script.js"></script>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">TESZT WEBOLDAL</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" title="#">MENÜPONT 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" title="#">MENÜPONT 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" title="#">MENÜPONT 3</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" title="#">MENÜPONT 4</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</body>
+
+</html>
---
+++ app/views/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.ckeditor.com/ckeditor5/29.2.0/classic/ckeditor.js"></script>
+</head>
+<body>
+<div id="editor">
+    <p>Here goes the initial content of the editor.</p>
+</div>
+<script>
+    ClassicEditor
+    .create( document.querySelector( '#editor' ) )
+    .then( editor => {
+        console.log( editor );
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
+</script>
+</body>
+</html>
