7f9957312e01c88b73c7feb9246507ef5065ee4a
Laszlo Gyurkovics <gyurkovics95@gmail.com>
2021-09-15 11:00:30 +0200

    log ready

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<?php
-
-use app\lib\router;
-use app\lib\request;
-use app\lib\response;
-
-router::get('/', function () {
-    echo 'Hello World';
-});
-
-router::get('/post/([0-9]*)', function (Request $req, Response $res) {
-    $res->toJSON([
-        'post' =>  ['id' => $req->params[0]],
-        'status' => 'ok'
-    ]);
-});
+<?php require __DIR__ . '/vendor/autoload.php';
+use App\Lib\App;
+App::run();
